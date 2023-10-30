64b5814aff5651f414f7d8faa06fa387cdfc8e06
anson <georgeanson@foxmail.com>
2023-10-30 14:44:05 +0800

    注册中心

---
+++ qrcb-register/src/main/java/com/alibaba/nacos/QrcbNacosApplication.java
@@ -0,0 +1,24 @@
+package com.alibaba.nacos;
+
+import com.alibaba.nacos.config.ConfigConstants;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @Author Anson
+ * @Create 2023-10-30
+ * @Description 注册中心 <br/>
+ */
+
+@EnableScheduling
+@SpringBootApplication
+public class QrcbNacosApplication {
+
+    public static void main(String[] args) {
+        System.setProperty(ConfigConstants.STANDALONE_MODE, "true");
+        System.setProperty(ConfigConstants.AUTH_ENABLED, "false");
+        System.setProperty(ConfigConstants.LOG_BASEDIR, "logs");
+        SpringApplication.run(QrcbNacosApplication.class, args);
+    }
+}
